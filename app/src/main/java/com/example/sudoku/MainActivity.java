b1c962a7151f76b37208a3dbf2cafa6d15516b91
Robert Spindler <robert.spindler@spacehuhn.tech>
2022-01-08 17:41:31 +0100

    commented out useless Code bits

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -7,21 +7,23 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button[][] buttons = new Button[3][3];
-    private boolean player1Turn = true;
+    private Button[][] buttons = new Button[9][9];
+    //private boolean player1Turn = true;
     private int roundCount;
-    private int player1Points;
-    private int player2Points;
-    private TextView textViewPlayer1;
-    private TextView textViewPlayer2;
+    //private int player1Points;
+    //private int player2Points;
+    //private TextView textViewPlayer1;
+    //private TextView textViewPlayer2;
+    private int selectedNum;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textViewPlayer1 = findViewById(R.id.text_view_p1);
-        textViewPlayer2 = findViewById(R.id.text_view_p2);
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        //textViewPlayer1 = findViewById(R.id.text_view_p1);
+        //textViewPlayer2 = findViewById(R.id.text_view_p2);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 String buttonID = "button_" + i + j;
                 int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
                 buttons[i][j] = findViewById(resID);
@@ -41,39 +43,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (!((Button) v).getText().toString().equals("")) {
             return;
         }
+        /*
         if (player1Turn) {
             ((Button) v).setText("X");
         } else {
             ((Button) v).setText("O");
         }
+        */
         roundCount++;
         if (checkForWin()) {
+            /*
             if (player1Turn) {
-                player1Wins();
+                playerWins();
             } else {
                 player2Wins();
             }
-        } else if (roundCount == 9) {
+             */
+        } else if (roundCount == 81) {
             draw();
         } else {
-            player1Turn = !player1Turn;
+            //player1Turn = !player1Turn;
         }
     }
-    private boolean checkForWin() {
-        String[][] field = new String[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+    private boolean checkForWin() { //TODO: an Sudoku anpassen
+        String[][] field = new String[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 field[i][j] = buttons[i][j].getText().toString();
             }
         }
-        for (int i = 0; i < 3; i++) {
+        /*
+        for (int i = 0; i < 9; i++) {
             if (field[i][0].equals(field[i][1])
                     && field[i][0].equals(field[i][2])
                     && !field[i][0].equals("")) {
                 return true;
             }
         }
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 9; i++) {
             if (field[0][i].equals(field[1][i])
                     && field[0][i].equals(field[2][i])
                     && !field[0][i].equals("")) {
@@ -90,58 +97,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 && !field[0][2].equals("")) {
             return true;
         }
+         */
         return false;
     }
-    private void player1Wins() {
-        player1Points++;
+    private void playerWins() {
+        //player1Points++;
         Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
-        updatePointsText();
+        //updatePointsText();
         resetBoard();
     }
+    /*
     private void player2Wins() {
         player2Points++;
         Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
         updatePointsText();
         resetBoard();
     }
+     */
     private void draw() {
         Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
         resetBoard();
     }
+    /*
     private void updatePointsText() {
         textViewPlayer1.setText("Player 1: " + player1Points);
         textViewPlayer2.setText("Player 2: " + player2Points);
     }
+     */
     private void resetBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 buttons[i][j].setText("");
             }
         }
         roundCount = 0;
-        player1Turn = true;
+        //player1Turn = true;
     }
     private void resetGame() {
-        player1Points = 0;
-        player2Points = 0;
-        updatePointsText();
+        //player1Points = 0;
+        //player2Points = 0;
+        //updatePointsText();
         resetBoard();
     }
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(Bundle outState) {   //this is what happens when the app closes TODO: an Sudoku anpassen
         super.onSaveInstanceState(outState);
         outState.putInt("roundCount", roundCount);
-        outState.putInt("player1Points", player1Points);
-        outState.putInt("player2Points", player2Points);
-        outState.putBoolean("player1Turn", player1Turn);
+        //outState.putInt("player1Points", player1Points);
+        //outState.putInt("player2Points", player2Points);
+        //outState.putBoolean("player1Turn", player1Turn);
     }
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {  //this is what happens when the app opens TODO: an Sudoku anpassen
         super.onRestoreInstanceState(savedInstanceState);
         roundCount = savedInstanceState.getInt("roundCount");
-        player1Points = savedInstanceState.getInt("player1Points");
-        player2Points = savedInstanceState.getInt("player2Points");
-        player1Turn = savedInstanceState.getBoolean("player1Turn");
+        //player1Points = savedInstanceState.getInt("player1Points");
+        //player2Points = savedInstanceState.getInt("player2Points");
+        //player1Turn = savedInstanceState.getBoolean("player1Turn");
     }
 }
 /*
